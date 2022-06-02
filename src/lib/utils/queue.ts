6e7076a9bff590e830143c085c6294a570fbd91f
m93a <m93a.cz@gmail.com>
2022-06-02 09:11:56 +0200

    removed @iter-tools as dependency

---
+++ src/lib/utils/queue.ts
@@ -0,0 +1,112 @@
+// Adopted from https://www.npmjs.com/package/@iter-tools/queue
+// Created by Conrad Buck, published under the MIT license
+
+
+class QueueItem<T> {
+  value: T | null
+  next: QueueItem<T> | null;
+
+  constructor(value: T | null) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+const _ = Symbol.for('_');
+const flag = Symbol.for('@iter-tools/queue');
+
+export default class Queue<T> {
+  [_]: { size: number, cleared: boolean, head: QueueItem<T>, tail: QueueItem<T> }
+
+  constructor(values?: Iterable<T>) {
+    this.clear();
+
+    // Mirrors the behavior of Map and Set
+    if (values != null) {
+      for (const value of values) {
+        this.push(value);
+      }
+    }
+  }
+
+  static isQueue(inst: any): inst is Queue<any> {
+    return inst != null && inst[flag];
+  }
+
+  get [flag]() {
+    return true;
+  }
+
+  get size() {
+    return this[_].size;
+  }
+
+  clear() {
+    const emptyItem = new QueueItem<T>(null);
+
+    if (this[_]) this[_].cleared = true;
+    this[_] = { size: 0, cleared: false, head: emptyItem, tail: emptyItem };
+  }
+
+  peek() {
+    if (this.size === 0) return undefined;
+    return this[_].head.next!.value;
+  }
+
+  shift() {
+    if (this.size === 0) return undefined;
+    const { head } = this[_];
+    const { value, next } = head.next!;
+    if (next === null) {
+      this[_].tail = head;
+    }
+    head.next = next;
+    this[_].size--;
+
+    return value;
+  }
+
+  push(value: T) {
+    const { tail } = this[_];
+    tail.next = this[_].tail = new QueueItem(value);
+    this[_].size++;
+  }
+
+  [Symbol.iterator]() {
+    return this.values();
+  }
+
+  *keys() {
+    const __ = this[_];
+    let item: QueueItem<T> | null = __.head;
+    let i = 0;
+    while ((item = item.next) !== null && !__.cleared) {
+      yield i++;
+    }
+  }
+
+  *values(): Generator<T> {
+    const __ = this[_];
+    let item: QueueItem<T> | null = __.head;
+    while ((item = item.next) !== null && !__.cleared) {
+      yield item.value!;
+    }
+  }
+
+  *entries(): Generator<[number, T]> {
+    const __ = this[_];
+    let item: QueueItem<T> | null = __.head;
+    let i = 0;
+    while ((item = item.next) !== null && !__.cleared) {
+      yield [i, item.value!];
+      i++;
+    }
+  }
+
+  forEach(cb: (value: T, index: number, queue: Queue<T>) => any, thisArg: any) {
+    if (thisArg != null) {
+      cb = cb.bind(thisArg);
+    }
+    for (const [key, value] of this.entries()) cb(value, key, this);
+  }
+}
